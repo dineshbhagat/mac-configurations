5b7e7751127b8c9d331f940825a06c8012cc7cce
DB <3823705+dineshbhagat@users.noreply.github.com>
2021-01-27 10:28:10 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ bundle install
 
 # To run inside container
 # bundle exec jekyll serve -w --host 0.0.0.0
+# host: http://0.0.0.0:4000/
 
 # To run in local
-bundle exec jekyll serve -w --host 0.0.0.0
+# host: localhost:4000
+bundle exec jekyll serve
